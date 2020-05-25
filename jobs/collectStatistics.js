8c87057fb367330922297528fbfd5832b9e4ee88
olegtsigulov <olegcigulev@gmail.com>
2020-05-25 08:34:24 +0300

    fix last currency

--- jobs/collectStatistics.js
+++ jobs/collectStatistics.js
@@ -1,12 +1,12 @@
 const { CronJob } = require('cron');
 const { currencyHelper } = require('utilities/helpers');
 
-exports.ordinaryStatisticsJob = new CronJob('0 */5  * * * *', async () => {
+exports.ordinaryStatisticsJob = new CronJob('*/5 * * * *', async () => {
   // add new currency statistic every 5 minutes
   await currencyHelper.collectStatistics('ordinaryData', 'coingecko');
 }, null, true, null, null, false);
 
-exports.dailyStatisticsJob = new CronJob('0 0 0 */1 * *', async () => {
+exports.dailyStatisticsJob = new CronJob('0 0 * * * ', async () => {
   // add new currency statistic every 1 day
   await currencyHelper.getDailyCurrency();
 }, null, true, null, null, false);
