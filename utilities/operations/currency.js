6d0265b982b1e1eb1383d24697c74def80accb55
olegtsigulov <olegcigulev@gmail.com>
2020-05-25 13:25:02 +0300

    add timestamps

--- utilities/operations/currency.js
+++ utilities/operations/currency.js
@@ -1,8 +1,12 @@
+const _ = require('lodash');
 const { currencyHelper } = require('utilities/helpers');
 
 exports.getCurrencies = async (data) => {
   const result = {};
   const { result: currentCurrency } = await currencyHelper.getCurrentCurrencies(data);
+  currentCurrency.type = _.get(currentCurrency, 'type') ? currentCurrency.type : 'ordinaryData';
+  currentCurrency.createdAt = _.get(currentCurrency, 'createdAt') ? currentCurrency.createdAt : new Date();
+  currentCurrency.updatedAt = _.get(currentCurrency, 'updatedAt') ? currentCurrency.updatedAt : new Date();
   result.current = currentCurrency;
   result.weekly = await currencyHelper.getWeaklyCurrencies(currentCurrency);
   return { result };
