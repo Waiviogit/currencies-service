9c856b8e291e86a58071ea06a85174219fd4d711
olegtsigulov <olegcigulev@gmail.com>
2020-05-26 09:18:20 +0300

    add timestamps

--- jobs/collectStatistics.js
+++ jobs/collectStatistics.js
@@ -6,7 +6,7 @@ exports.ordinaryStatisticsJob = new CronJob('*/5 * * * *', async () => {
   await currencyHelper.collectStatistics('ordinaryData', 'coingecko');
 }, null, true, null, null, false);
 
-exports.dailyStatisticsJob = new CronJob('0 0 * * * ', async () => {
+exports.dailyStatisticsJob = new CronJob('0 0 */1 * * ', async () => {
   // add new currency statistic every 1 day
   await currencyHelper.getDailyCurrency();
 }, null, true, null, null, false);
