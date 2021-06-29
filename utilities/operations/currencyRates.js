332f6531f0e63200f426e3a4d768ed9891c505cb
Boris Andrzhiievskyi <borees2@gmail.com>
2021-06-29 17:35:48 +0300

    fixLatest

--- utilities/operations/currencyRates.js
+++ utilities/operations/currencyRates.js
@@ -4,6 +4,7 @@ const { currenciesRateModel } = require('models');
 const _ = require('lodash');
 
 exports.getCurrencyRatesLatest = async ({ base, symbols }) => {
+  if (base === symbols.toString()) return { rates: { [base]: 1 } };
   const selectRates = _.map(symbols, (el) => ({ [`rates.${el}`]: 1 }));
 
   const { result: { rates = {} } } = await currenciesRateModel.findOne({
