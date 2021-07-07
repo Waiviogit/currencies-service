51d0732b4dcf879f45be72ed49c663ad0f4f5802
Boris Andrzhiievskyi <borees2@gmail.com>
2021-07-07 11:24:05 +0300

    Git-2825

--- constants/serviceData.js
+++ constants/serviceData.js
@@ -6,6 +6,14 @@ exports.allowedCurrencies = ['usd', 'btc'];
 exports.SUPPORTED_CURRENCIES = {
   USD: 'USD',
   CAD: 'CAD',
+  EUR: 'EUR',
+  AUD: 'AUD',
+  MXN: 'MXN',
+  GBP: 'GBP',
+  JPY: 'JPY',
+  CNY: 'CNY',
+  RUB: 'RUB',
+  UAH: 'UAH',
 };
 
 exports.BASE_CURRENCIES = [
@@ -14,6 +22,14 @@ exports.BASE_CURRENCIES = [
 
 exports.RATE_CURRENCIES = [
   this.SUPPORTED_CURRENCIES.CAD,
+  this.SUPPORTED_CURRENCIES.EUR,
+  this.SUPPORTED_CURRENCIES.AUD,
+  this.SUPPORTED_CURRENCIES.MXN,
+  this.SUPPORTED_CURRENCIES.GBP,
+  this.SUPPORTED_CURRENCIES.JPY,
+  this.SUPPORTED_CURRENCIES.CNY,
+  this.SUPPORTED_CURRENCIES.RUB,
+  this.SUPPORTED_CURRENCIES.UAH,
 ];
 
 exports.CURRENCY_RATE_API = {
