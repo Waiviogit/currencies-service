de58d1a07a128f76afe0fc04281a0df17ce89bb1
fesmofet <borees2@gmail.com>
2022-04-14 10:37:00 +0300

    add engineProxy

--- constants/serviceData.js
+++ constants/serviceData.js
@@ -69,3 +69,11 @@ exports.STATISTIC_RECORD_TYPES = {
   ORDINARY: 'ordinaryData',
   DAILY: 'dailyData',
 };
+
+exports.HIVE_ENGINE_NODES = [
+  'https://api.hive-engine.com/rpc', // Germany
+  'https://api2.hive-engine.com/rpc', // Finland
+  'https://herpc.dtools.dev', // Miami
+  'https://us.engine.rishipanthee.com', // Finland
+  'https://ha.herpc.dtools.dev', // New Jersey
+];
--- utilities/hiveEngine/engineQuery.js
+++ utilities/hiveEngine/engineQuery.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const _ = require('lodash');
 
-module.exports = async ({
+const { HIVE_ENGINE_NODES } = require('../../constants/serviceData');
+
+exports.engineQuery = async ({
   hostUrl = 'https://api.hive-engine.com/rpc',
   method = 'find',
   params,
@@ -23,4 +25,41 @@ module.exports = async ({
   } catch (error) {
     return { error };
   }
+};
+
+exports.engineProxy = async ({
+  hostUrl,
+  method,
+  params,
+  endpoint,
+  id,
+  attempts = 5,
+}) => {
+  const response = await this.engineQuery({
+    hostUrl,
+    method,
+    params,
+    endpoint,
+    id,
+  });
+  if (_.has(response, 'error')) {
+    if (attempts <= 0) return response;
+    return this.engineProxy({
+      hostUrl: getNewNodeUrl(hostUrl),
+      method,
+      params,
+      endpoint,
+      id,
+      attempts: attempts - 1,
+    });
+  }
+  return response;
+};
+
+const getNewNodeUrl = (hostUrl) => {
+  const index = hostUrl ? HIVE_ENGINE_NODES.indexOf(hostUrl) : 0;
+
+  return index === HIVE_ENGINE_NODES.length - 1
+    ? HIVE_ENGINE_NODES[0]
+    : HIVE_ENGINE_NODES[index + 1];
 };
