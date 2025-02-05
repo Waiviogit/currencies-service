b78eb69eaeaf7f90dc3e235ee6bab84fea09ec3c
fesmofet <borees2@gmail.com>
2025-02-05 17:26:43 +0200

    remove body parser

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const bodyParser = require('body-parser');
 const { routes } = require('routes');
 const swaggerDocument = require('swagger');
 require('jobs');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 app.listen(process.env.PORT || 8001);
